576df788b8e3e91d45198a721fcc4a2839444a50
HelioStrike <rageofepicfury@gmail.com>
2019-02-15 17:21:19 +0530

    committed

--- 1111B.cpp
+++ 1111B.cpp
@@ -1,25 +1,26 @@
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i = (a); i < (b); i++)
+#define pii pair<int,int>
+#define fi first
+#define se second
 #define ll long long
-#define MAXN 100010
+#define MAXN 200010
 using namespace std;
 
 
-int n,m,k,a[MAXN],b[MAXN];
+int n,m,k,c,arr[MAXN];
+priority_queue<pair<int,int> > pq;
 ll tot;
-
-bool cmp(int b1,int b2) { return (a[b1]>a[b2]); }
 
 int main()
 {
-    scanf("%d%d%d",&n,&m,&k); FOR(i,1,n+1) scanf("%d",&a[i]),b[i]=i;
-    sort(b+1,b+n+1,cmp);
-    sort(b+1,b+m*k+1);
+    scanf("%d%d%d",&n,&m,&k); FOR(i,1,n+1) { scanf("%d",&c); pq.push({c,i}); }
 
-    FOR(i,1,m*k+1) tot+=a[b[i]];
-    printf("%lli\n",tot);
+    FOR(i,0,m*k) { tot+=pq.top().fi; arr[i]=pq.top().se; pq.pop(); }
+    sort(arr,arr+m*k);
 
-    for(int i=m;i<m*k;i+=m) printf("%d ",b[i]);
+    cout << tot << '\n';
+    for(int i=m-1;i<m*k-1;i+=m) printf("%d ",arr[i]);
     printf("\n");
 
     return 0;
